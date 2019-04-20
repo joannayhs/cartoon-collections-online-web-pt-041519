f7171b4b5e1fe774698f6364c570a4cb6b9bcdf4
Joanna Yuhas <joannayuhas@gmail.com>
2019-04-20 22:43:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.collect.compact do |cheese|
+  cheese_types.collect.flatten do |cheese|
    if array.include?(cheese) 
     return cheese
    else 
