05978bccd1afd67b7bd7ac624ee1c1f6feaadd0d
Joanna Yuhas <joannayuhas@gmail.com>
2019-04-20 22:42:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.collect do |cheese|
+  cheese_types.collect.compact do |cheese|
    if array.include?(cheese) 
     return cheese
    else 
