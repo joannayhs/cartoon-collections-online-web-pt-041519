f5069ebb40f923935c04c2b47c69f43349c45153
Joanna Yuhas <joannayuhas@gmail.com>
2019-04-20 22:37:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,10 +19,7 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.each do |list_item|
-    cheese_types.each do |cheese|
-      if list_item == cheese 
-        return list_item 
+    cheese_types.include?(list_item)
+     return list_item
       end
-    end 
-  end
 end
